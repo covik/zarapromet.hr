85c435ea50cc42635efb214544c0bca1a6ba5c39
Mate Nakić <mate.nakic@zth.dev>
2024-09-10 16:01:52 +0000

    install storybook

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import custom from './scripts/no-emotion-tagged-template.js';
+import storybook from 'eslint-plugin-storybook';
 
 export default tseslint.config(
     { ignores: ['dist'] },
@@ -18,6 +19,7 @@ export default tseslint.config(
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
             custom,
+            storybook,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
